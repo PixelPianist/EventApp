98189928265623a334e3b6cf0b87d660b0bd9b12
Rachel Keirouz <rachel.keirouz@gmail.com>
2015-10-26 22:21:46 -0700

    Created a new activity to add an event
    Initial implementation of the activity to add the event. The java file
    has nothing, but the xml file has the formatting set up.

---
+++ app/src/main/java/com/eventappucsd/activity/EnterEventInfoActivity.java
@@ -0,0 +1,13 @@
+package com.eventappucsd.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterEventInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_event_info);
+    }
+}
