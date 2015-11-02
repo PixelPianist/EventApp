aecf4ec1f3fc3a4ab283cd1d7b75b9000f1a95ae
Pete <ppanourg@ucsd.edu>
2015-11-02 10:45:40 -0800

    fakedb

---
+++ app/src/main/java/com/eventappucsd/backend/FakeDB.java
@@ -0,0 +1,31 @@
+package com.eventappucsd.backend;
+
+import com.eventappucsd.activity.MainActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Pete on 10/31/15.
+ *
+ * A fake DB for testing purposes.
+ */
+public class FakeDB extends MainActivity {
+
+    private static ArrayList<Event> eventCheat = new ArrayList<>();
+    static int dbSize = 0;
+
+    public static void add(Event currentEvent){
+
+        eventCheat.add(currentEvent);
+
+    }
+
+    public static void printEvent(int index){
+        //Just printing the event name for now because I'm lazy.
+        System.out.println(eventCheat.get(index).getEventName());
+    }
+
+    public static String getEventName(int index){
+        return eventCheat.get(index).getEventName();
+    }
+}
